8baa1e90cce1313622e4c7e4549c73e0e3d57881
Foday C. Koroma <koromafoday840@gmail.com>
2026-05-24 13:22:18 +0000

    feat: count uppercase and lowercase characters

--- practice.c
+++ practice.c
@@ -1,15 +1,30 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 int main(void)
-{  
+{
+    int counter = 0;
+    int small = 0;
+    char text[] = "Hello, Foday";
+    int length = strlen(text);
+    printf("The number of characters in the text is %i\n", length);
+    for (int i = 0; i < length; i++)
+    if (isalpha(text[i]))
+    {  
+        if (isupper(text[i]))
+        {  
+            counter++;
+        }
+        else if (islower(text[i]))
+        {
+            small++;
+        }
+        
+        
+    }
+    printf("the number of  capital letters of the text is %i\n", counter);
+    printf("the number of small letters of the text is %i\n", small);
+        
+}
 
-char text[] = "Hello, world!";
-int length = strlen(text);
-printf("The text is %i characters long \n\n", length);
-for(int i = 0; i < length; i++)
-{  
-    char symbol = text[i];
-    printf("Character at index %i is: %c\n", i, symbol);
-}
-}
