74a9d45aae0e54d52841b6432eae8a89d52ae86b
Foday C. Koroma <koromafoday840@gmail.com>
2026-05-25 14:54:38 +0000

    feat: implement raw user input pipeline using fgets without helper libraries

--- practice.c
+++ practice.c
@@ -1,30 +1,13 @@
-#include <string.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(void)
-{
-    int counter = 0;
-    int small = 0;
-    char text[] = "Hello, Foday";
-    int length = strlen(text);
-    printf("The number of characters in the text is %i\n", length);
-    for (int i = 0; i < length; i++)
-    if (isalpha(text[i]))
-    {  
-        if (isupper(text[i]))
-        {  
-            counter++;
-        }
-        else if (islower(text[i]))
-        {
-            small++;
-        }
-        
-        
-    }
-    printf("the number of  capital letters of the text is %i\n", counter);
-    printf("the number of small letters of the text is %i\n", small);
-        
-}
+{  
 
+char text[1000];
+printf("Text: ");
+fflush(stdout);
+fgets(text, sizeof(text), stdin);
+printf("Your input was %s\n", text);
+}
