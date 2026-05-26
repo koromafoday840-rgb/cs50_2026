77817548fd707a0d6fab59ac3099f9c1b03a45d3
Foday C. Koroma <koromafoday840@gmail.com>
2026-05-26 21:52:36 +0000

    feat: build standard C string analysis for words, letters and sentences

--- practice.c
+++ practice.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
+#include <math.h>
 
 int main(void)
-{  
+{   
+    char buffer[1000];
+    printf("Text: ");
+    fflush(stdout);
+    fgets(buffer, sizeof(buffer), stdin);
+    int letters = 0;
+    int words = 1;
+    int sentences = 0; 
+    for (int i = 0; buffer[i] != '\0'; i++)
+    {
+        if (isalpha(buffer[i]))
+        {
+            letters++;
+        }
+        if (buffer[i] == ' ')
+        {
+            words++;
+        }
+        if (buffer[i] == '.' || buffer[i] == '!' || buffer[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    printf("letters: %i\n", letters);
+    printf("words: %i\n", words);
+    printf("sentences: %i\n", sentences);
 
-char text[1000];
-printf("Text: ");
-fflush(stdout);
-fgets(text, sizeof(text), stdin);
-printf("Your input was %s\n", text);
+        
+    
+
+
 }
