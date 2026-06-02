f813695b558f4c1b9874abc531abbae9b7df4815
Foday C. Koroma <koromafoday840@gmail.com>
2026-06-02 11:37:29 +0000

    refactor: fix argv syntax, isolate key length and defensive guardrails

--- check/check.c
+++ check/check.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char argv[])
 {
-    char first[40];
-    printf("input your key: ");
-    fflush("stdout"):
-    fgets(key, sizeof(key, stdin));
-    int key = atoi(argv[1]);
-    c = (p - base + k) (% 26) + base;
+    char key[40];
+    printf("Enter your key: ");
+    fflush(stdout);
+    if (!isdigit(argv[1][i]))
+    {
+        printf("Usage: ./caesar\n");
+        return 1;
+    }
+    int key = strlen(agrv[1])
+    for (int i = 0; i < key; i++ )
+
     
 
     
