8a7bf5b7206e85334986a5c8c3a5e885a311947f
Foday C. Koroma <koromafoday840@gmail.com>
2026-06-03 09:44:37 +0000

    refactor: rename check.c to caesar.c for assignment specification

--- readability.c
+++ readability.c
@@ -1,52 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <math.h>
 #include <string.h>
 
 int main(void)
-{   
-    char buffer[1000];
-    printf("Text: ");
-    fflush(stdout);
-    fgets(buffer, sizeof(buffer), stdin);
-    int letters = 0;
-    int words = 0;
-    int sentences = 0; 
-    if (strlen(buffer) > 0)
+{
+    char *name = "Foday";
+    int length = strlen(name);
+    for (int i = 0; i < length; i++)
     {
-        words = 1;
+        printf("%i = %c\n", i, name[i]);
     }
-    for (int i = 0; buffer[i] != '\0'; i++)
-    {
-        if (isalpha(buffer[i]))
-        {
-            letters++;
-        }
-        if (buffer[i] == ' ')
-        {
-            words++;
-        }
-        if (buffer[i] == '.' || buffer[i] == '!' || buffer[i] == '?')
-        {
-            sentences++;
-        }
-    }    
-         
-        float L = ((float)letters/words) * 100;
-        float S = ((float)sentences/words) * 100;
-        float index = 0.0588 * L - 0.0296 * S - 15.8;
-        int grade = round(index);
-        if (grade < 1)
-        {
-            printf("Before Grade 1\n");
-        }
-        else if (grade > 16)
-        {
-            printf("Grade 16+\n");
-        }
-        else
-        {     
-            printf("Grade %i\n", grade);
-        }
 }
