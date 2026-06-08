4c59aea28fc977350ae24aec81ccaa718f4bd001
Foday C. Koroma <koromafoday840@gmail.com>
2026-06-08 13:53:17 +0000

    feat: complete local implementation of caesar cipher logic

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -20,65 +20,32 @@ int main(int argc, char *argv[])
             printf("Usage: ./caesar key");
             return 1;
         }
-    }    
-    int key = atoi(argv[1]);
-    printf("Enter your plaintext: ");
-    if (isupper(*argv[1]))
-    {
-        c = (p - 65 + key) (% 26) + 65;
     }
-    else if (islower(*argv[1]))
-    {
-        c = (p - 97 + key) (% 26) + 97;
-    }
+    int key = atoi(argv[1]);    
     char plaintext[1000];
     printf("Enter your plaintext: ");
     fflush(stdout);
     fgets(plaintext, sizeof(plaintext), stdin);
-    printf(ciphertext);
-    int plaintext_text = strlen(plaintext);
-    for (int i = 0; i < )
-    if (isupper(plaintext))
-    {
-        char c = (p - 'A' + ) % 26 + 'A';
-        printf("%c", c);
-    }
-    else if (islower(plaintext))
-    {
-        char c = (p - 'a' + key) % 26 + 'a';
-    }
-    printf("%c", c);
-    else 
-    plaintext == " " || plaintext == " ! " || plaintext == " ? ";
-    {
-        printf("%c", p)
-    }
-    for (int = 0; plaintext[i] != '\0'; i++)
+    printf("ciphertext: ");
+    for (int i = 0; plaintext[i] != '\0'; i++)
+
     {  
         char p = plaintext[i];
-    }
-if (isupper(p));
+    
+if (isupper(p))
 {
-    c = (p - 'A' + key) % 26 + 'A';
+    char c = (p - 'A' + key) % 26 + 'A';
     printf("%c", c);
 }
 else if (islower(p))
 {
-    c = (p - 'a' + key) % 26 + 'a';
+   char c = (p - 'a' + key) % 26 + 'a';
     printf("%c", c);
 }
 else
 { 
-    printf("%c", c);
+    printf("%c", p);
 }
-
-
-
-
-
-
-
-
-
+    }
+printf("\n");
 }
-
