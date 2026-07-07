9707ae3adc3e83d3ac3ce319144026fac1d0db39
Foday C. Koroma <koromafoday840@gmail.com>
2026-07-07 11:44:50 +0000

    feat: my first attempt of writing merge sort program

---
+++ merge.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void merge_sort(int array[], int left, int right)
+void merge(int array[], int left, int mid, int right)
+
+int main(void)
+
+int foday[] {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, };
+int n = 20;
+
+merge_sort(foday, 0, n - 1);
+
+printf("The sorted numbers are: ");
+for (int i = 0; i < n; i++)
+{    
+    printf("i% ", foday[i]);
+}
+printf("\n");
+return 0;
+}
