a63ea26162e50aff9fea7e31fe485db0b1100002
Foday C. Koroma <koromafoday840@gmail.com>
2026-07-08 13:44:05 +0000

    feat: implement merge_sort manager function

--- merge.c
+++ merge.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-void merge_sort(int array[], int left, int right)
-void merge(int array[], int left, int mid, int right)
+void merge_sort(int array[], int left, int right);
+void merge(int array[], int left, int mid, int right);
 
 int main(void)
-
-int foday[] {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, };
+{ 
+int foday[] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 int n = 20;
 
 merge_sort(foday, 0, n - 1);
@@ -12,8 +12,24 @@ merge_sort(foday, 0, n - 1);
 printf("The sorted numbers are: ");
 for (int i = 0; i < n; i++)
 {    
-    printf("i% ", foday[i]);
+    printf("%i ", foday[i]);
 }
 printf("\n");
 return 0;
 }
+void merge_sort(int array[], int left, int right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+    int mid = left + (right - left) / 2;
+     merge_sort(array, left, mid);
+     merge_sort(array, mid + 1, right);
+     merge(array, left, mid, right );
+}
+void merge(int array[], int left, int mid, int right)
+{
+
+}
+
