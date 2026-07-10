c1ff68dd07fdf8ab85ec11cadd1b89ce5f5340a8
Foday C. Koroma <koromafoday840@gmail.com>
2026-07-10 21:22:17 +0000

    feat: create temp arrays and add left / right copy loops

--- merge.c
+++ merge.c
@@ -33,4 +33,15 @@ void merge(int array[], int left, int mid, int right)
 
 int n1 = mid - left + 1;
 int n2 = right - mid;
+int L[n1];
+int R[n2];
+
+for (int i = 0; i < n1; i++)
+{
+    L[i] = array[left + i];
+}
+for (int j = 0; j < n2; j++)
+{
+    R[j] = array[mid + 1 + j];
+}
 }
