93c86ef0c35f7cc612ab0340a2387a2bdc3983d0
Foday C. Koroma <koromafoday840@gmail.com>
2026-07-30 10:23:54 +0000

    feat: add candidate setup and command_line argument

--- plurality.c
+++ plurality.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #define MAX 9
 
 bool vote(char *name);
@@ -11,29 +12,42 @@ typedef struct
     char *name;
     int vote;
 }candidate;
-
+candidate candidates[MAX];
+    int candidate_count;
+bool vote(char *name);
+void print_winner(void);
 int main(int argc, char *argv[])
 {
-    candidate candidates[MAX];
-    int candidate_count;
+    
     if (argc < 2)
     {
         printf("Usage: plurality[candidate..]\n");
         return 1;
     }
-    for (int i = 0; i < candidate_count; i++)
-    {  
-    candidates[i].name = argv[i] + 1;
     int candidate_count = argc - 1;
-    
-    candidate_count = argc - 1;
-    if(candidate_count > MAX)
+    if (candidate_count > MAX)
     {
-        printf("Maximum number of candidate is %i", MAX);
+        printf("The maximum number of candidates is %i\n", MAX);
         return 2;
     }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].vote = 0;
     }
+    int voter_count;
 
+    printf("Enter the number of voters: ");
+    fgets(int voter_count, sizeof(voter_count), stdin);
+
+
+
+
+
+
+    
+
+}
 
 
 
