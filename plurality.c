fd17cabe2da6aaf77b8a6de04d481ba541f1c915
Foday C. Koroma <koromafoday840@gmail.com>
2026-08-03 10:03:18 +0000

    feat: implement standard C plurality voting logic and fix function scope

--- plurality.c
+++ plurality.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
         printf("Usage: plurality[candidate..]\n");
         return 1;
     }
-    int candidate_count = argc - 1;
+    candidate_count = argc - 1;
     if (candidate_count > MAX)
     {
         printf("The maximum number of candidates is %i\n", MAX);
@@ -33,10 +33,70 @@ int main(int argc, char *argv[])
         candidates[i].name = argv[i + 1];
         candidates[i].votes = 0;
     }
-    int voter_count;
-
+    char buffer[10];
     printf("Enter the number of voters: ");
-    fgets(int voter_count, sizeof(voter_count), stdin);
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+        buffer[strcspn(buffer, "\n")] = '\0';
+    }
+    int voter_count = atoi(buffer);
+    for (int i =0; i < voter_count; i++)
+    {
+        char name_buffer[50];
+        printf("Vote: ");
+        if (fgets(name_buffer, sizeof(name_buffer), stdin) != NULL)
+        {
+            name_buffer[strcspn(name_buffer, "\n")] = '\0';
+        }
+        if (!vote(name_buffer))
+        {
+            printf("Invalid vote");
+        }
+    }
+    print_winner();
+    return 0;
+}    
+    bool vote(char *name)
+    {
+        for (int i = 0; i < candidate_count; i++)
+        {
+            if (strcmp(candidates[i].name, name) == 0)
+            {
+                candidates[i].votes++;
+                return true;
+            }
+        }
+        return false;
+    }
+    void print_winner(void)
+    {
+        int max_votes = 0; 
+        for (int i = 0; i < candidate_count; i++)
+        {
+            if (candidates[i].votes > max_votes)
+            {   
+                max_votes = candidates[i].votes;
+            }
+        }
+        if (max_votes > 0)
+        {  
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("The winner of the election is %s\n", candidates[i].name);
+        }
+    }
+}
+
+    else
+    {
+        printf("No valid vote cast.\n");
+    }
+        
+}
+
+
 
 
 
@@ -45,7 +105,6 @@ int main(int argc, char *argv[])
 
     
 
-}
 
 
 
