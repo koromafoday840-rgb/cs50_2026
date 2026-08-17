119c865b9827c226cbc4973431403a7e3f8a1bfd
Foday C. Koroma <koromafoday840@gmail.com>
2026-08-17 21:51:26 +0000

    wip: begin is_tie implementation

--- runoff.c
+++ runoff.c
@@ -19,6 +19,8 @@ bool print_winner(void);
 
 int find_min(void);
 
+bool is_tie(void);
+
 bool vote(int voter, int rank, char *name);
 
 candidate candidates[MAX];
@@ -101,7 +103,7 @@ int main(int argc, char *argv[])
      }
     int find_min()
     {
-        int least_votes = 0;
+        int least_votes = max_voters;
         for (int i = 0; i < candidate_count; i++)
         {
             if (candidates[i].eliminated == false)
@@ -113,6 +115,24 @@ int main(int argc, char *argv[])
 
             }
         }
-        
+        return least_votes;
     }
-    
+   bool is_tie()
+   {
+        for (int i = 0; i < candidate_count; i++)
+        {
+            if (candidates[i].eliminated == true)
+            {
+                if (candidates[i].votes == least_votes)
+                {
+                    printf("Tie");
+                    return true;
+                }
+                else
+                {
+                    printf("No tie");
+                    return false;
+                }
+            }
+        }
+   } 
